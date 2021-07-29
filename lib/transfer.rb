b562658f4bb18ae8eea24698d2897624fefac923
Motti789 <mchillywilly@gmail.com>
2021-07-29 14:21:18 -0400

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,14 +1,22 @@
 require 'pry'
 class Transfer
 
-  attr_accessor :name, :transfer, :amount, :status
+  attr_accessor :receiver, :amount, :status, :sender
 
-  def initialize(name, transfer, amount)
-    @name = name
-    @transfer = transfer
+  def initialize(sender, receiver, amount)
+    @receiver = receiver
     @amount = amount
     @status = "pending"
+    @sender = sender
   end  
+
+  def valid?
+    sender.valid? && receiver.valid?
+  end  
+
+  def execute_transaction(sender, receiver, amount)
+  end 
+
   
 end
 
