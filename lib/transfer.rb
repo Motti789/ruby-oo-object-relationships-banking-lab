e83a9fd4de3313d75b6768c152f7b0e4e57ecba9
Motti789 <mchillywilly@gmail.com>
2021-07-29 13:58:59 -0400

    Done.

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,9 +1,15 @@
 require 'pry'
 class Transfer
 
+  attr_accessor :name, :transfer, :amount, :status
 
-  def initialize
-    
+  def initialize(name, transfer, amount)
+    @name = name
+    @transfer = transfer
+    @amount = amount
+    @status = "pending"
   end  
   
 end
+
+
